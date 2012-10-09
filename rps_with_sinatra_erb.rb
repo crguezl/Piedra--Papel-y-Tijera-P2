c66a127c34448d7c0dc3ac58d53a474248c63c7b
Sara Rijo Garcia <sararijogarcia@gmail.com>
2012-10-09 18:22:28 +0100

    Practica PPT terminada

--- rps_with_sinatra_erb.rb
+++ rps_with_sinatra_erb.rb
@@ -5,26 +5,36 @@ require 'erb'
 # and set up an array of viable moves that a player (and the
 # computer) can perform
 before do
-  @defeat = { rock: :scissors, paper: :rock, scissors: :paper}
+  @defeat = { piedra: :tijera, papel: :piedra, tijera: :papel}
   @throws = @defeat.keys
 end
 
-get '/throw/:type' do
-  # the params hash stores querystring and form data
-  @player_throw = params[:type].to_sym
+get '/' do
+  erb :vista_inicial
+end
+ 
+get '/*' do
+  redirect '/'
+end
 
-  halt(403, "You must throw one of the following: '#{@throws.join(', ')}'") unless @throws.include? @player_throw
+post '/throw' do
+ 	# the params hash stores querystring and form data
+	if params[:type]== nil
+		halt erb :error
+	else
+  		@player_throw = params[:type].to_sym
+  end
 
   @computer_throw = @throws.sample
 
   if @player_throw == @computer_throw 
-    @answer = "There is a tie"
+    @answer = "EMPATE"
     erb :index
   elsif @player_throw == @defeat[@computer_throw]
-    @answer = "Computer wins; #{@computer_throw} defeats #{@player_throw}"
+    @answer = "Has perdido -->  #{@computer_throw}  gana a #{@player_throw}"
     erb :index
   else
-    @answer = "Well done. #{@player_throw} beats #{@computer_throw}"
+    @answer = "Has ganado --> #{@player_throw}  gana a  #{@computer_throw}"
     erb :index
   end
 end
